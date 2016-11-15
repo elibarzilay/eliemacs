69f451f0e5829c483155481805f60a65dcc449f4
Eli Barzilay <eli@barzilay.org>
2016-11-15 01:23:47 -0500

    Get rid of some overrides that are no longer needed.

--- include/overrides.el
+++ include/overrides.el
@@ -3,105 +3,6 @@
 ;; Written by Eli Barzilay: Maze is Life!   (eli@barzilay.org)
 
 ;;-----------------------------------------------------------------------------
-;; needed to make the simple.el behavior do what cua-mode used to do
-;; (bug reported, fixed in repo so should be dropped in the future)
-(make-variable-buffer-local 'transient-mark-mode)
-
-;;-----------------------------------------------------------------------------
-;; Get new version from "subr.el" to get the new exitfun return value
-;;   (No other changes, beyond the lexical-let)
-
-(defun set-transient-map (map &optional keep-pred on-exit)
-  "Set MAP as a temporary keymap taking precedence over other keymaps.
-Normally, MAP is used only once, to look up the very next key.
-However, if the optional argument KEEP-PRED is t, MAP stays
-active if a key from MAP is used.  KEEP-PRED can also be a
-function of no arguments: it is called from `pre-command-hook' and
-if it returns non-nil, then MAP stays active.
-
-Optional arg ON-EXIT, if non-nil, specifies a function that is
-called, with no arguments, after MAP is deactivated.
-
-This uses `overriding-terminal-local-map' which takes precedence over all other
-keymaps.  As usual, if no match for a key is found in MAP, the normal key
-lookup sequence then continues.
-
-This returns an \"exit function\", which can be called with no argument
-to deactivate this transient map, regardless of KEEP-PRED."
-  (let* ((clearfun (make-symbol "clear-transient-map"))
-         (exitfun
-          (lambda ()
-            (internal-pop-keymap map 'overriding-terminal-local-map)
-            (remove-hook 'pre-command-hook clearfun)
-            (when on-exit (funcall on-exit)))))
-    ;; Don't use letrec, because equal (in add/remove-hook) would get trapped
-    ;; in a cycle.
-    (fset clearfun
-          (lambda ()
-            (with-demoted-errors "set-transient-map PCH: %S"
-              (unless (cond
-                       ((null keep-pred) nil)
-                       ((not (eq map (cadr overriding-terminal-local-map)))
-                        ;; There's presumably some other transient-map in
-                        ;; effect.  Wait for that one to terminate before we
-                        ;; remove ourselves.
-                        ;; For example, if isearch and C-u both use transient
-                        ;; maps, then the lifetime of the C-u should be nested
-                        ;; within isearch's, so the pre-command-hook of
-                        ;; isearch should be suspended during the C-u one so
-                        ;; we don't exit isearch just because we hit 1 after
-                        ;; C-u and that 1 exits isearch whereas it doesn't
-                        ;; exit C-u.
-                        t)
-                       ((eq t keep-pred)
-                        (eq this-command
-                            (lookup-key map (this-command-keys-vector))))
-                       (t (funcall keep-pred)))
-                (funcall exitfun)))))
-    (add-hook 'pre-command-hook clearfun)
-    (internal-push-keymap map 'overriding-terminal-local-map)
-    exitfun))
-
-;;-----------------------------------------------------------------------------
-;; Override from "simple.el"
-
-;; Add the last line that fixes transposes with a negative argument.
-;; (Patch reported and committed, so this should go away eventually.)
-(defun transpose-subr (mover arg &optional special)
-  "Subroutine to do the work of transposing objects.
-Works for lines, sentences, paragraphs, etc.  MOVER is a function that
-moves forward by units of the given object (e.g. forward-sentence,
-forward-paragraph).  If ARG is zero, exchanges the current object
-with the one containing mark.  If ARG is an integer, moves the
-current object past ARG following (if ARG is positive) or
-preceding (if ARG is negative) objects, leaving point after the
-current object."
-  (let ((aux (if special mover
-	       (lambda (x)
-		 (cons (progn (funcall mover x) (point))
-		       (progn (funcall mover (- x)) (point))))))
-	pos1 pos2)
-    (cond
-     ((= arg 0)
-      (save-excursion
-	(setq pos1 (funcall aux 1))
-	(goto-char (or (mark) (error "No mark set in this buffer")))
-	(setq pos2 (funcall aux 1))
-	(transpose-subr-1 pos1 pos2))
-      (exchange-point-and-mark))
-     ((> arg 0)
-      (setq pos1 (funcall aux -1))
-      (setq pos2 (funcall aux arg))
-      (transpose-subr-1 pos1 pos2)
-      (goto-char (car pos2)))
-     (t
-      (setq pos1 (funcall aux -1))
-      (goto-char (car pos1))
-      (setq pos2 (funcall aux arg))
-      (transpose-subr-1 pos1 pos2)
-      (goto-char (+ (car pos2) (- (cdr pos1) (car pos1)))))))) ; ELI
-
-;;-----------------------------------------------------------------------------
 ;; Overrides from "help.el", and showing temporary buffers
 ;; (Works better than ehelp.)
 
@@ -230,7 +131,7 @@ Do not call this in the scope of `with-help-window'."
 (defun ask-user-about-supersession-threat (fn)
   "(DON'T!) Ask a user who is about to modify an obsolete buffer what to do.
 This function has two choices: it can return, in which case the modification
-of the buffer will proceed, or it can (signal 'file-supersession (file)),
+of the buffer will proceed, or it can (signal \\='file-supersession (file)),
 in which case the proposed buffer modification will not be made.
 
 You can rewrite this to use any criterion you like to choose which one to do.
@@ -265,30 +166,6 @@ THIS IS A MODIFIED VERSION THAT AUTOMATICALLY RELOADS THE FILE."
 ))
 
 )
-
-;;-----------------------------------------------------------------------------
-;; Override from "files.el": fix a bug in this function (reported, and
-;; should be resolved in a future version)
-
-(defun set-file-extended-attributes (filename attributes)
-  "Set extended attributes of file FILENAME to ATTRIBUTES.
-
-ATTRIBUTES must be an alist of file attributes as returned by
-`file-extended-attributes'.  Value is t if the function succeeds
-in setting all of the given attributes excluding ones that
-indicate \"no information\"."
-  (let ((result t))
-    (dolist (elt attributes)
-      (let ((attr (car elt))
-            (val (cdr elt)))
-        (unless (cond ((eq attr 'acl)
-                       (or (equal val nil)
-                           (set-file-acl filename val)))
-                      ((eq attr 'selinux-context)
-                       (or (equal val '(nil nil nil nil))
-                           (set-file-selinux-context filename val))))
-          (setq result nil))))
-    result))
 
 ;;-----------------------------------------------------------------------------
 ;; Override from "files.el": don't ask about reverting a buffer, just say it
@@ -343,14 +220,15 @@ the various files."
 	      (or nowarn
 		  find-file-suppress-same-file-warnings
 		  (string-equal filename (buffer-file-name other))
-		  (message "%s and %s are the same file"
-			   filename (buffer-file-name other)))
+		  (files--message "%s and %s are the same file"
+                                  filename (buffer-file-name other)))
 	      ;; Optionally also find that buffer.
 	      (if (or find-file-existing-other-name find-file-visit-truename)
 		  (setq buf other))))
 	;; Check to see if the file looks uncommonly large.
 	(when (not (or buf nowarn))
-	  (abort-if-file-too-large (nth 7 attributes) "open" filename))
+	  (abort-if-file-too-large (nth 7 attributes) "open" filename)
+	  (warn-maybe-out-of-memory (nth 7 attributes)))
 	(if buf
 	    ;; We are using an existing buffer.
 	    (let (nonexistent)
@@ -486,63 +364,6 @@ Do you want to revisit the file normally now? ")
 )))
 
 ;;-----------------------------------------------------------------------------
-;; Override from "files.el": make this respect case-folding
-
-' ; looks like there's no need for this anymore--?
-(defun file-expand-wildcards (pattern &optional full)
-  "Expand wildcard pattern PATTERN.
-This returns a list of file names which match the pattern.
-
-If PATTERN is written as an absolute file name,
-the values are absolute also.
-
-If PATTERN is written as a relative file name, it is interpreted
-relative to the current default directory, `default-directory'.
-The file names returned are normally also relative to the current
-default directory.  However, if FULL is non-nil, they are absolute."
-  (save-match-data
-    (let* ((nondir (file-name-nondirectory pattern))
-	   (dirpart (file-name-directory pattern))
-	   ;; A list of all dirs that DIRPART specifies.
-	   ;; This can be more than one dir
-	   ;; if DIRPART contains wildcards.
-	   (dirs (if (and dirpart
-			  (string-match "[[*?]"
-					(or (file-remote-p dirpart 'localname)
-					    dirpart)))
-		     (mapcar 'file-name-as-directory
-			     (file-expand-wildcards (directory-file-name dirpart)))
-		   (list dirpart)))
-	   contents)
-      (while dirs
-	(when (or (null (car dirs))	; Possible if DIRPART is not wild.
-		  (and (file-directory-p (directory-file-name (car dirs)))
-		       (file-readable-p (car dirs))))
-	  (let ((this-dir-contents
-		 ;; Filter out "." and ".."
-		 (delq nil
-		       (mapcar #'(lambda (name)
-				   (let ((n (file-name-nondirectory name)))
-                                     (and (not (member n '("." "..")))
-                                          ;; ELI: do the wildcard filtering
-                                          ;; here, not in directory files which
-                                          ;; doesn't respect case folding
-                                          (string-match (wildcard-to-regexp nondir)
-                                                        n)
-                                          name)))
-			       (directory-files (or (car dirs) ".") full
-						(wildcard-to-regexp nondir))))))
-	    (setq contents
-		  (nconc
-		   (if (and (car dirs) (not full))
-		       (mapcar (function (lambda (name) (concat (car dirs) name)))
-			       this-dir-contents)
-		     this-dir-contents)
-		   contents))))
-	(setq dirs (cdr dirs)))
-      contents)))
-
-;;-----------------------------------------------------------------------------
 ;; Override from "isearch.el": Also, make scroll keys simply leave isearch if
 ;; the point goes out of the screen.
 
@@ -605,19 +426,25 @@ If N is negative, search forwards for the -Nth following match."
 (add-^-to-interactive-command backward-page)
 
 (eval-after-load "dired" '(progn
-
-(add-^-to-interactive-command dired-next-line)
-(add-^-to-interactive-command dired-previous-line)
-(add-^-to-interactive-command dired-next-dirline)
-(add-^-to-interactive-command dired-prev-dirline)
-
-))
+  (add-^-to-interactive-command dired-next-dirline)
+  (add-^-to-interactive-command dired-prev-dirline)))
 
 ;;-----------------------------------------------------------------------------
-;; Override from "server.el": fix a bug, don't ask questions for
-;; unmodified buffers.
+;; Override from "server.el": don't ask questions for unmodified buffers.
 
 (eval-after-load "server" '(progn
+
+(defun server-kill-emacs-query-function ()
+  "Ask before exiting Emacs if it has live clients."
+  (or (not (let (live-client)
+             (dolist (proc server-clients)
+               ;; ELI: also check modifications and ignore unmodified buffers
+               (when (memq t (mapcar (lambda (buf) (and (buffer-live-p buf)
+                                                        (buffer-modified-p buf)))
+                                     (process-get proc 'buffers)))
+                 (setq live-client t)))
+             live-client))
+      (yes-or-no-p "This Emacs session has clients; exit anyway? ")))
 
 (defun server-kill-buffer-query-function ()
   "Ask before killing a server buffer."
@@ -632,20 +459,6 @@ If N is negative, search forwards for the -Nth following match."
          res)
       (yes-or-no-p (format "Buffer `%s' still has clients; kill it? "
 			   (buffer-name (current-buffer))))))
-
-(defun server-kill-emacs-query-function ()
-  "Ask before exiting Emacs if it has live clients."
-  (or (let (live-client)
-	(dolist (proc server-clients)
-          ;; ELI: also check modifications and ignore unmodified buffers
-	  (when (memq t (mapcar (lambda (buf) (and (buffer-live-p buf)
-                                                   (buffer-modified-p buf)))
-                                (process-get proc 'buffers)))
-	    (setq live-client t)))
-        ;; ELI: fix a (reported) bug, also no need for the first
-        ;; condition as a result
-        (not live-client))
-      (yes-or-no-p "This Emacs session has clients; exit anyway? ")))
 
 ))
 
@@ -689,7 +502,11 @@ enable the mode if ARG is omitted or nil.
 
 When Delete Selection mode is enabled, typed text replaces the selection
 if the selection is active.  Otherwise, typed text is just inserted at
-point regardless of any selection."
+point regardless of any selection.  Also, commands that normally delete
+just one character will delete the entire selection instead.
+
+See `delete-selection-helper' and `delete-selection-pre-hook' for
+information on adapting behavior of commands in Delete Selection mode."
   :global t :group 'editing-basics
   (if (not delete-selection-mode)
       (progn (remove-hook 'pre-command-hook 'delete-selection-pre-hook)
